6ee92b8b7891fa54017627aee9ef301c183fe5fc
DazzS <49374948+DazzS@users.noreply.github.com>
2020-11-27 19:24:13 +0300

    1

---
+++ precache-manifest.aec4076c9ede527e972f0f022074019a.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a4687ff8638b58bb9e",
+    "url": "/css/chunk-08445450.b3153ee8.css"
+  },
+  {
+    "revision": "e95d7809afd3c7f1788b",
+    "url": "/css/chunk-0d9491f7.b3153ee8.css"
+  },
+  {
+    "revision": "4bd395a776d286c12efd",
+    "url": "/css/chunk-1e1f0172.6a36876f.css"
+  },
+  {
+    "revision": "74dc5c6195f57c161226",
+    "url": "/css/chunk-36311dae.1cbd1584.css"
+  },
+  {
+    "revision": "2761d10720199b3f3ca7",
+    "url": "/css/chunk-41844a3e.2810468c.css"
+  },
+  {
+    "revision": "41224a82770b0867aece",
+    "url": "/css/chunk-49baf526.6a36876f.css"
+  },
+  {
+    "revision": "ee2d09a2e3c526754563",
+    "url": "/css/chunk-4a25acd7.6a36876f.css"
+  },
+  {
+    "revision": "3ec734656b92b9cd2ab0",
+    "url": "/css/chunk-52291179.b3153ee8.css"
+  },
+  {
+    "revision": "efd64a6caff4892f5c8c",
+    "url": "/css/chunk-6ec5ad70.75c03278.css"
+  },
+  {
+    "revision": "b1adc54efa502c05b82e",
+    "url": "/css/chunk-ff9c66a4.a7aab4f7.css"
+  },
+  {
+    "revision": "d1f33437b68575324d54",
+    "url": "/css/chunk-vendors.0539acf9.css"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/fonts/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/fonts/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/fonts/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/fonts/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "84c35acb1f9c6ddc63c6af4ae464acb3",
+    "url": "/img/category1.84c35acb.png"
+  },
+  {
+    "revision": "7cf9eae7c2573c41014075da4fe6c1bf",
+    "url": "/img/category2.7cf9eae7.png"
+  },
+  {
+    "revision": "f3b7b8f6924355df7288b70135f0bcf6",
+    "url": "/img/category3.f3b7b8f6.png"
+  },
+  {
+    "revision": "c0ce76f4a568a40b1858434c1f7702a9",
+    "url": "/img/category4.c0ce76f4.png"
+  },
+  {
+    "revision": "0d8e28d58657f8f4517e917d0d20aa99",
+    "url": "/img/cold_plate_1.0d8e28d5.jpg"
+  },
+  {
+    "revision": "5021e5baf770ae0de582992186ff5ccc",
+    "url": "/img/cold_plate_2.5021e5ba.jpg"
+  },
+  {
+    "revision": "3a5053e5ccb91076cd581e87a57f6f4a",
+    "url": "/img/cold_plate_3.3a5053e5.jpg"
+  },
+  {
+    "revision": "9c910b9395a87c8797177681edd5121e",
+    "url": "/img/cold_plates_4.9c910b93.jpg"
+  },
+  {
+    "revision": "3877747dcdf65879110821fedd258986",
+    "url": "/img/cold_recept_1.3877747d.jpg"
+  },
+  {
+    "revision": "a88985ff35b278396d3af307abe91a06",
+    "url": "/img/cold_recept_2.a88985ff.jpg"
+  },
+  {
+    "revision": "364de762f5615f86649b865ceeca0ce1",
+    "url": "/img/cold_recept_3.364de762.jpg"
+  },
+  {
+    "revision": "87e28a19c5a47efaeca021ee380e78c9",
+    "url": "/img/cold_recept_4.87e28a19.jpg"
+  },
+  {
+    "revision": "7820b01b8e68ef7c08c40965553bc00b",
+    "url": "/img/cold_recept_5.7820b01b.jpg"
+  },
+  {
+    "revision": "a70f7ce1358e6742aed2493862b61a6d",
+    "url": "/img/cold_recept_6.a70f7ce1.jpg"
+  },
+  {
+    "revision": "4450463eefe47a96e33d57a4f5387785",
+    "url": "/img/cold_recept_7.4450463e.jpg"
+  },
+  {
+    "revision": "151262248e082180551e709dac61b3d4",
+    "url": "/img/cold_recept_8.15126224.jpg"
+  },
+  {
+    "revision": "a9fd879ef2af295cb43fe05daac29104",
+    "url": "/img/cold_recept_9.a9fd879e.jpg"
+  },
+  {
+    "revision": "7ec2548fdecba321acd0bc76c968420b",
+    "url": "/img/desert_plate_1.7ec2548f.jpg"
+  },
+  {
+    "revision": "7f9b92d25b99a5be01b77e838963a857",
+    "url": "/img/desert_plate_2.7f9b92d2.jpg"
+  },
+  {
+    "revision": "6ad647a36b247dc3840a86db92f3f286",
+    "url": "/img/desert_plate_3.6ad647a3.jpg"
+  },
+  {
+    "revision": "3d31330d74d60791560075f53cf46391",
+    "url": "/img/desert_plate_4.3d31330d.jpg"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/img/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "f9844046298cd1053024ca92bef5cb31",
+    "url": "/img/hot_plate_1.f9844046.jpg"
+  },
+  {
+    "revision": "d603ff44b59a8c67ad71e1c815fc5d1b",
+    "url": "/img/hot_plate_2.d603ff44.jpg"
+  },
+  {
+    "revision": "b0ff7f6dea94bff16da00eafc5f163ee",
+    "url": "/img/hot_plate_3.b0ff7f6d.jpg"
+  },
+  {
+    "revision": "1223322443da93207d223adff72f5382",
+    "url": "/img/hot_plate_4.12233224.jpg"
+  },
+  {
+    "revision": "2eb19f61bf47349110cd70580868ebf2",
+    "url": "/img/hot_recept_1.2eb19f61.jpg"
+  },
+  {
+    "revision": "e818c45dea942c7f83287e334384c0cf",
+    "url": "/img/hot_recept_10.e818c45d.jpg"
+  },
+  {
+    "revision": "ca64d8e10b5c41df156e30efe6f0d393",
+    "url": "/img/hot_recept_11.ca64d8e1.jpg"
+  },
+  {
+    "revision": "528ce780789dac66cf5fabbc0d33321e",
+    "url": "/img/hot_recept_12.528ce780.jpg"
+  },
+  {
+    "revision": "34c193f6f072a56baf260e9fc4d3557c",
+    "url": "/img/hot_recept_2.34c193f6.jpg"
+  },
+  {
+    "revision": "6a9eb4a6b9dd9ae3cb27aace5f7b1855",
+    "url": "/img/hot_recept_3.6a9eb4a6.jpg"
+  },
+  {
+    "revision": "87772396bfa3bd5a8f0e2efe76800372",
+    "url": "/img/hot_recept_4.87772396.jpg"
+  },
+  {
+    "revision": "1dceafcfed9f8695d6cbd5ffa4fe01a1",
+    "url": "/img/hot_recept_5.1dceafcf.jpg"
+  },
+  {
+    "revision": "4b94d447699677cf9c2513d7d51dbd93",
+    "url": "/img/hot_recept_6.4b94d447.jpg"
+  },
+  {
+    "revision": "aab1266485d78697bb097a2b2dab9ca0",
+    "url": "/img/hot_recept_7.aab12664.jpg"
+  },
+  {
+    "revision": "664badde33d44a1eb0e1e494ae4db229",
+    "url": "/img/hot_recept_8.664badde.jpg"
+  },
+  {
+    "revision": "91e35bc362e2d2b1fc2ac0a9dca7e36f",
+    "url": "/img/hot_recept_9.91e35bc3.jpg"
+  },
+  {
+    "revision": "dae304bdbbff3f6b1aa2d8ef20b4a20f",
+    "url": "/img/logo.dae304bd.png"
+  },
+  {
+    "revision": "2aaa3915520dd27f4e230d13771c2382",
+    "url": "/img/snack_plate_1.2aaa3915.jpg"
+  },
+  {
+    "revision": "7e1f13bd4faedda19b4540072875e859",
+    "url": "/img/snack_plate_2.7e1f13bd.jpg"
+  },
+  {
+    "revision": "fa318d7ec27319a38b14c2c22485c546",
+    "url": "/img/snack_plate_3.fa318d7e.jpg"
+  },
+  {
+    "revision": "65bc009466f8358e5a3be8e865d4447c",
+    "url": "/img/snack_plate_4.65bc0094.jpg"
+  },
+  {
+    "revision": "7b6c825ceaaa2d7ef07b021edb0022ea",
+    "url": "/img/snack_recept_1.7b6c825c.jpg"
+  },
+  {
+    "revision": "d44b51aaeb88e4d29bccfb3932f14adb",
+    "url": "/img/snack_recept_2.d44b51aa.jpg"
+  },
+  {
+    "revision": "c3130131cd11182e8a8b6ef8929fa951",
+    "url": "/img/snack_recept_3.c3130131.jpg"
+  },
+  {
+    "revision": "1c0d6654440944ffbcdb6ad2578e2442",
+    "url": "/img/snack_recept_4.1c0d6654.jpg"
+  },
+  {
+    "revision": "4a0932aa936473880c6743d27030d438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c58a48f333ed8295cf44",
+    "url": "/js/app.7450d70e.js"
+  },
+  {
+    "revision": "c8a4687ff8638b58bb9e",
+    "url": "/js/chunk-08445450.3e4052f6.js"
+  },
+  {
+    "revision": "e95d7809afd3c7f1788b",
+    "url": "/js/chunk-0d9491f7.97aa296d.js"
+  },
+  {
+    "revision": "4bd395a776d286c12efd",
+    "url": "/js/chunk-1e1f0172.4cec7160.js"
+  },
+  {
+    "revision": "74dc5c6195f57c161226",
+    "url": "/js/chunk-36311dae.1a46c605.js"
+  },
+  {
+    "revision": "2761d10720199b3f3ca7",
+    "url": "/js/chunk-41844a3e.b170eab0.js"
+  },
+  {
+    "revision": "41224a82770b0867aece",
+    "url": "/js/chunk-49baf526.b55b94a9.js"
+  },
+  {
+    "revision": "ee2d09a2e3c526754563",
+    "url": "/js/chunk-4a25acd7.99d07b36.js"
+  },
+  {
+    "revision": "3ec734656b92b9cd2ab0",
+    "url": "/js/chunk-52291179.b46bb54f.js"
+  },
+  {
+    "revision": "efd64a6caff4892f5c8c",
+    "url": "/js/chunk-6ec5ad70.2f7147d7.js"
+  },
+  {
+    "revision": "b1adc54efa502c05b82e",
+    "url": "/js/chunk-ff9c66a4.6a01b929.js"
+  },
+  {
+    "revision": "d1f33437b68575324d54",
+    "url": "/js/chunk-vendors.e86f0a52.js"
+  },
+  {
+    "revision": "2d045cf64ca68a45675c5b894360e5c6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
